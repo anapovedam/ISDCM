4c729b476c8d8f1ede7ca277671923aff0779f83
anapovedam <anapoveda0609@gmail.com>
2025-05-10 20:11:58 +0200

    fichero Resource provisional

--- src/main/java/isdcm/webappreproduccionvideos/resources/JakartaEE91Resource.java
+++ src/main/java/isdcm/webappreproduccionvideos/resources/JakartaEE91Resource.java
@@ -3,6 +3,13 @@ package isdcm.webappreproduccionvideos.resources;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 
 /**
  *
@@ -16,5 +23,37 @@ public class JakartaEE91Resource {
         return Response
                 .ok("ping Jakarta EE")
                 .build();
+    }
+    
+    /**
+     * Sample of GET method
+     * @param info
+     * @param fecha
+     * @return 
+     */
+    @Path("getInfo")
+    @GET    
+    @Produces("text/html")
+    public String getInfo (@QueryParam("info") String info, 
+                            @QueryParam("fecha") String fecha) {
+        
+        return "<html><head></head> <body> Informaci&oacute;n recibida " + info + " en fecha " + fecha + " </body></html>";
+    }
+
+    /**
+     * Sample of POST method
+     * 
+     * @param info
+     * @param fecha
+     * @return 
+     */
+    @Path("postInfo")   
+    @POST    
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/html")
+    public String postInfo (  @FormParam("info") String info, 
+                              @FormParam("fecha") String fecha) 
+    {                
+        return "<html><head></head> <body> Informaci&oacute;n recibida " + info + " en fecha " + fecha + " </body></html>";
     }
 }
